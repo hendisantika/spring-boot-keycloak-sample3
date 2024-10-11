4c0e8b226a35b2a336a4466b1feefea5d3b8b93e
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 06:24:41 +0700

    Add convert in JwtConverter🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloak/security/JwtConverter.java
+++ src/main/java/id/my/hendisantika/keycloak/security/JwtConverter.java
@@ -2,9 +2,15 @@ package id.my.hendisantika.keycloak.security;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -27,4 +33,11 @@ public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken>
         this.properties = properties;
     }
 
+    @Override
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
+                extractResourceRoles(jwt).stream()).collect(Collectors.toSet());
+        return new JwtAuthenticationToken(jwt, authorities, getPrincipalClaimName(jwt));
+    }
 }

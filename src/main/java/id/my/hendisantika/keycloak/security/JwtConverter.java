d4464484053e83020c70812eb3857724b0f493d6
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 06:24:27 +0700

    Add JwtConverter🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/keycloak/security/JwtConverter.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.keycloak.security;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/24
+ * Time: 06.23
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+    private final JwtConverterProperties properties;
+
+    public JwtConverter(JwtConverterProperties properties) {
+        this.properties = properties;
+    }
+
+}

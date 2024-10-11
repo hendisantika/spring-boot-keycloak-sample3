d25e830289337f391d2b0bfa998f4628bcadca12
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 06:24:54 +0700

    Add getPrincipalClaimName in JwtConverter🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloak/security/JwtConverter.java
+++ src/main/java/id/my/hendisantika/keycloak/security/JwtConverter.java
@@ -4,6 +4,7 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimNames;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.stereotype.Component;
@@ -39,5 +40,13 @@ public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken>
                 jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
                 extractResourceRoles(jwt).stream()).collect(Collectors.toSet());
         return new JwtAuthenticationToken(jwt, authorities, getPrincipalClaimName(jwt));
+    }
+
+    private String getPrincipalClaimName(Jwt jwt) {
+        String claimName = JwtClaimNames.SUB;
+        if (properties.getPrincipalAttribute() != null) {
+            claimName = properties.getPrincipalAttribute();
+        }
+        return jwt.getClaim(claimName);
     }
 }

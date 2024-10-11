b52106cab1f3e7391111aafeefc657467574af65
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 06:18:38 +0700

    Create Spring Boot App 🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/keycloak/SpringBootKeycloakSample3Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.keycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeycloakSample3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKeycloakSample3Application.class, args);
+    }
+
+}

bdada6ec9690dc27578418b11f73c54d8fbe7adb
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 06:25:08 +0700

    Add extractResourceRoles in JwtConverter🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/keycloak/security/JwtConverter.java
+++ src/main/java/id/my/hendisantika/keycloak/security/JwtConverter.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.keycloak.security;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimNames;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
@@ -10,6 +11,8 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -48,5 +51,20 @@ public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken>
             claimName = properties.getPrincipalAttribute();
         }
         return jwt.getClaim(claimName);
+    }
+
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        Map<String, Object> resource;
+        Collection<String> resourceRoles;
+
+        if (resourceAccess == null
+                || (resource = (Map<String, Object>) resourceAccess.get(properties.getResourceId())) == null
+                || (resourceRoles = (Collection<String>) resource.get("roles")) == null) {
+            return Set.of();
+        }
+        return resourceRoles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toSet());
     }
 }

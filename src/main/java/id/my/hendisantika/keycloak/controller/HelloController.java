45ce2de379a747f183f52820e7143b00f7128089
hendisantika <hendisantika@yahoo.co.id>
2024-10-12 06:26:06 +0700

    Add HelloController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/keycloak/controller/HelloController.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.keycloak.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-keycloak-sample3
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/10/24
+ * Time: 06.25
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+}
